dc5b0e2221f4e5e4e32a778361d2ed60bdd0a4c7
BlockGem <kifeanyi216@gmail.com>
2024-02-16 00:40:17 +0100

    Variable Types

---
+++ freestyle.js
@@ -0,0 +1,10 @@
+// Variables and Types
+var myName = "John";
+var age = 20;
+var city = "San Francisco";
+var hobbies = ["Baseball", "football"];
+console.log("My name is ".concat(myName, " and I am ").concat(age, ". I live in ").concat(city, " and my hobbies are ").concat(hobbies));
+var NumberOrNull;
+NumberOrNull = 3;
+NumberOrNull = null;
+console.log(NumberOrNull);
--- freestyle.ts
+++ freestyle.ts
@@ -4,7 +4,10 @@ let age: number = 20;
 let city: string = "San Francisco";
 let hobbies: string[] = ["Baseball", "football"];
 
+console.log(`My name is ${myName} and I am ${age}. I live in ${city} and my hobbies are ${hobbies}`);
+
 let NumberOrNull: number | null;
 
 NumberOrNull = 3;
 NumberOrNull = null;
+console.log(NumberOrNull);
