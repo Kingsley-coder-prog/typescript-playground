db847df885d34798489e371377b91f03d771277c
BlockGem <kifeanyi216@gmail.com>
2024-02-16 00:03:45 +0100

    Initial Commit

---
+++ freestyle.ts
@@ -0,0 +1,10 @@
+// Variables and Types
+let myName: string = "John";
+let age: number = 20;
+let city: string = "San Francisco";
+let hobbies: string[] = ["Baseball", "football"];
+
+let NumberOrNull: number | null;
+
+NumberOrNull = 3;
+NumberOrNull = null;
