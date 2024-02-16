25b441a9e2b30783c131fc28734e2664b7ee6486
BlockGem <kifeanyi216@gmail.com>
2024-02-16 01:37:10 +0100

    Loops in TypeScript

--- freestyle.ts
+++ freestyle.ts
@@ -1,10 +1,10 @@
 // Variables and Types
-let myName: string = "John";
-let age: number = 20;
-let city: string = "San Francisco";
-let hobbies: string[] = ["Baseball", "football"];
+// let myName: string = "John";
+// let age: number = 20;
+// let city: string = "San Francisco";
+// let hobbies: string[] = ["Baseball", "football"];
 
-console.log(`My name is ${myName} and I am ${age}. I live in ${city} and my hobbies are ${hobbies}`);
+// console.log(`My name is ${myName} and I am ${age}. I live in ${city} and my hobbies are ${hobbies}`);
 
 let NumberOrNull: number | null;
 
@@ -23,9 +23,44 @@ function add(a: number, b: number): number {
 const newAdd = (a: number, b: number): number => {
   return a + b;
 };
-newAdd(5, 6);
+// console.log(newAdd(5, 6));
 
 const crdd = (a: number, b: number) => {
   return a * b;
 };
-crdd(2, 4);
+// console.log(crdd(2, 4));
+
+// Loops
+
+// for loop
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// for...of loop
+const fruits = ["apple", "orange", "banana"];
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+// for...in loop
+// const person = {
+//   newName: "John",
+//   newAge: 34,
+//   city: "New York"
+// };
+
+// for (let key in person) {
+//   console.log(key, person[key]);
+// }
+const person: any = {
+  name: "John",
+  age: 24,
+  city: "New York"
+};
+
+for (let key in person) {
+  //   console.log(key);
+  console.log(key, person[key]);
+}
