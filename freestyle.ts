40f7275d473f83ee2eadaef8856cbcfaa511a8e1
BlockGem <kifeanyi216@gmail.com>
2024-02-18 16:52:34 +0100

    Index Types

--- freestyle.ts
+++ freestyle.ts
@@ -226,3 +226,15 @@ function wrapInArray<T>(value: T): T[] {
 }
 
 console.log(wrapInArray(42));
+
+// Index Type
+interface MyDictionary {
+  [index: string]: string | number;
+}
+
+function getValueFromDict(key: string, dict: MyDictionary): string | number | undefined {
+  return dict[key];
+}
+
+const dict = { name: "Alice", age: 30 };
+console.log(getValueFromDict("name", dict));
