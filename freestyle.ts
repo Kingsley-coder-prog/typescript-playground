24ec213d826888f3d5a5b40bffe476c7ce1d0935
Ifeanyi Kingsley <kifeanyi216@gmail.com>
2024-02-28 23:08:15 +0100

    Refactoring CalculateArea Function

--- freestyle.ts
+++ freestyle.ts
@@ -303,7 +303,7 @@ type Shape = Circle | Square
 function calculateArea(shape: Shape): number {
   switch(shape.kind) {
     case "circle":
-      return Math.PI * shape.radius ** 3
+      return Math.PI * shape.radius ** 2
     case "square":
       return shape.sideLength ** 2
   }
