da0890e713d24bc9d4a61b5326de79632dc424b3
Ifeanyi Kingsley <kifeanyi216@gmail.com>
2024-03-02 17:54:24 +0100

    Hello Restructure

--- freestyle.ts
+++ freestyle.ts
@@ -157,7 +157,7 @@ console.log(`${myCar.year} ${myCar.model}`);
 
 // You can also export default  export
 export default function Component(): string {
-  return "hello!";
+  return "hellow!";
 }
 
 // Type Assertion
